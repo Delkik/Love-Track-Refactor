80f03974c924b21ee86b51c7e3774f1d275fc2d4
Imtiaz Rahman <32941495+imtiaznyc1@users.noreply.github.com>
2022-12-15 17:17:11 -0500

    updated code

--- client/src/pages/music.js
+++ client/src/pages/music.js
@@ -8,7 +8,8 @@ import { useSelector } from "react-redux";
 import io from "socket.io-client"
 import Chat from "../components/ChatBox";
 import { Navigate } from "react-router-dom";
-
+import filled from "../images/filled_social_media_button.png"
+import social from "../images/social_media_button.png"
 
 
 
@@ -17,7 +18,7 @@ export default function Music(){
     let user_data = useSelector(state => state.user.value)
     const navigate = useNavigate()
     const [clicked, setClicked] = useState(false)
-    const [leftChat, setLeftChat] = useState(false)
+    const [fav, setFav] = useState(false)
     const [tracks, setTracks] = useState(useSelector(state => state.songs.value))
     const [socketI, setSocket] = useState("")
     const [potUsers, setPot] = useState(useSelector(state => state.potentials.value))
@@ -39,6 +40,15 @@ export default function Music(){
           });  
         setSocket(socket)
     }
+
+    const favClick = () => {
+        if (fav){
+            setFav(false)
+        }else{
+            setFav(true)
+        }
+    }
+
     useEffect(() => {
         // console.log("music page tracks")
         // console.log(tracks)
@@ -52,7 +62,6 @@ export default function Music(){
     }, [])
     const onClickX = () =>{
         setClicked(false)
-        setLeftChat(true)
         navigate("/home",)
     }
 
@@ -70,7 +79,7 @@ export default function Music(){
             <h2 className="music-name">{potUsers[0]["name"]}, {potUsers[0]["age"]}</h2>
             <div className="music-albumCover">
                 <img src={tracks[0]['image']} alt="Album cover" className="music-cover"/>
-                <p>Song Name: {tracks[0]['name']}</p>
+                <p className="songName">{tracks[0]['name']}</p>
             </div>
             {/* <Link to="/chat"> */}
             <img 
@@ -87,6 +96,7 @@ export default function Music(){
             {/* <Chat socket = {socketI}/> */}
             <Chat name = {user_data["user"]["name"]} matchedName = {potUsers[0]["name"]} ownerId = {user_data["user"]["spotify_id"]} matchedId = {potUsers[0]["spotify_id"]} duration = {tracks[0]['duration']}/>
             {/* <img className = "xButton" onClick={() => onClickX()} src={xButton} alt="White Button"/> */}
+            <img onClick = {() => favClick()} className="like" src = {fav ? filled : social} />
             </div>
             }
 
